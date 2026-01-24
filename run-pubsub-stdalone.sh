f5a025b788221191babdeed2765add1387517ca8
Michel Albonico <bsd.albonico@gmail.com>
2026-01-24 15:44:38 +0100

    Update run-pubsub-stdalone.sh

--- run-pubsub-stdalone.sh
+++ run-pubsub-stdalone.sh
@@ -72,50 +72,50 @@ if [[ -f "$file" ]]; then
     ps -fC $client_name
     echo "Client PID: $LISTENER_PID"
     
-    # echo "Running PowerJoular"
-    # /usr/bin/powerjoular -p $TALKER_PID -f 'energy-server-powerjoular.csv' &
-    # /usr/bin/powerjoular -p $LISTENER_PID -f 'energy-client-powerjoular.csv' &
+    echo "Running PowerJoular"
+    /usr/bin/powerjoular -p $TALKER_PID -f 'energy-server-powerjoular.csv' &
+    /usr/bin/powerjoular -p $LISTENER_PID -f 'energy-client-powerjoular.csv' &
 
-    # CPU=0.0
-    # MEM=0
-    # CPU_L=0.0
-    # MEM_L=0
+    CPU=0.0
+    MEM=0
+    CPU_L=0.0
+    MEM_L=0
 
-    # spent_time=0
+    spent_time=0
 
-    # while kill -0 $SERVER_PID 2> /dev/null || kill -0 $CLIENT_PID 2> /dev/null || "$(echo "$spent_time < $etime" | bc)" -eq 1; do
-    #     TIME=$(date +%s)
-    #     CURRENT_CPU_PS=`ps -C $server_name -o %cpu | tail -1 | grep [0-9]`
-    #     CURRENT_CPU=`top -bn1 | grep $server_name | tail -1 | awk '{print $9}'`
-    #     CURRENT_CPU=`echo $CURRENT_CPU | sed 's/,/./g'`
-    #     CURRENT_CPU_L_PS=`ps -C $client_name -o %cpu | tail -1 | grep [0-9]`
-    #     CURRENT_CPU_L=`top -bn1 | grep $client_name | tail -1 | awk '{print $9}'` 
-    #     CURRENT_CPU_L=`echo $CURRENT_CPU_L | sed 's/,/./g'`
-    #     CURRENT_MEM=`pmap $SERVER_PID | head -3 | tail -1 | awk '{ print $2 }' | sed 's/K//'`
-    #     CURRENT_MEM_L=`pmap $CLIENT_PID | head -4 | tail -1 | awk '{ print $2 }' | sed 's/K//'`
-    #     CPU=`python3 -c "print (float($CURRENT_CPU))"`
-    #     CPU_L=`python3 -c "print (float($CURRENT_CPU_L))"`
-    #     MEM=`python3 -c "print (float($CURRENT_MEM))"`
-    #     MEM_L=`python3 -c "print (float($CURRENT_MEM_L))"`
+    while kill -0 $SERVER_PID 2> /dev/null || kill -0 $CLIENT_PID 2> /dev/null || "$(echo "$spent_time < $etime" | bc)" -eq 1; do
+        TIME=$(date +%s)
+        CURRENT_CPU_PS=`ps -C $server_name -o %cpu | tail -1 | grep [0-9]`
+        CURRENT_CPU=`top -bn1 | grep $server_name | tail -1 | awk '{print $9}'`
+        CURRENT_CPU=`echo $CURRENT_CPU | sed 's/,/./g'`
+        CURRENT_CPU_L_PS=`ps -C $client_name -o %cpu | tail -1 | grep [0-9]`
+        CURRENT_CPU_L=`top -bn1 | grep $client_name | tail -1 | awk '{print $9}'` 
+        CURRENT_CPU_L=`echo $CURRENT_CPU_L | sed 's/,/./g'`
+        CURRENT_MEM=`pmap $SERVER_PID | head -3 | tail -1 | awk '{ print $2 }' | sed 's/K//'`
+        CURRENT_MEM_L=`pmap $CLIENT_PID | head -4 | tail -1 | awk '{ print $2 }' | sed 's/K//'`
+        CPU=`python3 -c "print (float($CURRENT_CPU))"`
+        CPU_L=`python3 -c "print (float($CURRENT_CPU_L))"`
+        MEM=`python3 -c "print (float($CURRENT_MEM))"`
+        MEM_L=`python3 -c "print (float($CURRENT_MEM_L))"`
         
-    #     echo "$COUNT,talker,$TIME,$CPU,$MEM" >> $d_folder/server-cpu-mem.csv
-    #     echo "$COUNT,listener,$TIME,$CPU_L,$MEM_L" >> $d_folder/client-cpu-mem.csv
+        echo "$COUNT,talker,$TIME,$CPU,$MEM" >> $d_folder/server-cpu-mem.csv
+        echo "$COUNT,listener,$TIME,$CPU_L,$MEM_L" >> $d_folder/client-cpu-mem.csv
 
-    #     echo "$COUNT,talker,$TIME,$CURRENT_CPU_PS" >> $d_folder/server-cpu.csv
-    #     echo "$COUNT,listener,$TIME,$CURRENT_CPU_L_PS" >> $d_folder/client-cpu.csv
-    #     sleep 0.1
-    #     spent_time=$(echo "$spent_time + 0.5" | bc)
-    #     if [ "$(echo "$spent_time > $etime" | bc)" -eq 1 ]; then
-    #       echo "Timeout!"
-    #       pkill -9 -f $server_name
-    #       pkill -9 -f $client_name
-    #       pkill -9 -f powerjoular
-    #     fi
-    # done 
-    # echo "Stopped"
-    # if [ "$(echo "$spent_time > $etime" | bc)" -eq 1 ]; then
-    #     echo "Timeout!"
-    # fi
+        echo "$COUNT,talker,$TIME,$CURRENT_CPU_PS" >> $d_folder/server-cpu.csv
+        echo "$COUNT,listener,$TIME,$CURRENT_CPU_L_PS" >> $d_folder/client-cpu.csv
+        sleep 0.1
+        spent_time=$(echo "$spent_time + 0.5" | bc)
+        if [ "$(echo "$spent_time > $etime" | bc)" -eq 1 ]; then
+          echo "Timeout!"
+          pkill -9 -f $server_name
+          pkill -9 -f $client_name
+          pkill -9 -f powerjoular
+        fi
+    done 
+    echo "Stopped"
+    if [ "$(echo "$spent_time > $etime" | bc)" -eq 1 ]; then
+        echo "Timeout!"
+    fi
     
     cp -f energy-* $d_folder
     rm -Rf energy-*
